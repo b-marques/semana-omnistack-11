8b52135190adbeb1475a1942abc33719a2ffeb4e
Bruno Marques <brunomn95@gmail.com>
2020-03-26 14:07:43 -0300

    Add backend code
    - Add and configure express.
    
    - Add and configure knex.
    
    - Add database sqlite3.
    
    - Add database migrations.
    
    - Add routes and controllers.
    
    - Add cors.
    
    - Add nodemon for hot reloading.

---
+++ backend/notes.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * GET: Buscar/listar uma informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtrso, paginação)
+ * Route Params: Parâmtreos utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * Bancos de dados
+ *
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
+/**
+ * Entidades:
+ *
+ * ONG
+ * Caso (Incident)
+ *
+ */
+
+/**
+ * Funcionalidades:
+ *
+ * Login de ONG
+ * Logout de ONG
+ * Cadastro de ONG
+ * Cadaatrar novos casos
+ * Deletar casos
+ * Listar casos específicos de uma ONG
+ * Listar todos os casos
+ * Entrar em contato com a ONG
+ */
